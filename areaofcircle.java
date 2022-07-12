3597132945e67652b2f0805c42c56efa1cd9c838
HarshalRPatil25 <107293253+HarshalRPatil25@users.noreply.github.com>
2022-07-12 15:14:48 +0530

    Create areaofcircle.java

---
+++ areaofcircle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class A_circle{
+    public static void main(String[] args){
+        System.out.println("Area of circle");
+        Scanner sc = new Scanner(System.in);
+     float pi = 3.14f ;
+     float r = sc.nextFloat();
+     float area = 2*3.14f*r ;
+     System.out.println(area);
+    }
+}
