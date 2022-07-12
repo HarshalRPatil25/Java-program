ea593078ce5509c3ba29e0ad027515327ee80510
HarshalRPatil25 <107293253+HarshalRPatil25@users.noreply.github.com>
2022-07-12 14:12:49 +0530

    Create percentage.java

---
+++ percentage.java
@@ -0,0 +1,17 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        float sub1=sc.nextFloat();
+          float sub2=sc.nextFloat();
+            float sub3=sc.nextFloat();
+              float sub5=sc.nextFloat();
+                float sub4=sc.nextFloat();
+                float sum=sub1+sub2+sub3+sub4+sub5;
+                float total = 650;
+                float percentage = (sum/total)*100;
+                System.out.println(+percentage+" %");
+    }
+}
