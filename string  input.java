d79b3f7154b35e12b04c5b9f141ecae8974148a3
HarshalRPatil25 <107293253+HarshalRPatil25@users.noreply.github.com>
2022-07-14 14:17:08 +0530

    Create string  input.java

---
+++ string  input.java
@@ -0,0 +1,9 @@
+//how to take sring input
+import java.util.Scanner;
+public class Stri_input{
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
+        String str=sc.next();
+        System.out.println(str);
+    }
+}
